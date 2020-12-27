0bc048e15672886882bda6b78d77b5ce938eae22
Moritz Franz <Moritz.Franz@outlook.com>
2020-12-27 20:35:40 +0100

    Setting basic structure for implementing QR24-algorithm. Creating random pose matrices for calibration. Setting basic structure for measuring procedure during calibration.
    Detailed list of changes:
    - adding robCalibration package
    - adding Constants class
    - adding QR24 class containing the following:
      -> local workspace definition
      -> number of measurements
      -> list of robot's pose matrices
      -> list of measured marker's pose matrices
      -> creating random pose-matrices
      -> measuring algorithm to collect marker's pose matrices

--- src/robprakt/Constants.java
+++ src/robprakt/Constants.java
@@ -16,4 +16,6 @@ public class Constants {
 	 * composite speed of Adept Viper S850 in mm/s
 	 */
 	final static public double MAX_COMPOSITE_SPEED = 7600; //composite speed of Adept Viper S850 in mm/s
+	
 }
+
