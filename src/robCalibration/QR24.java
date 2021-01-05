dce8f7477c51ca49da293f7fa63d5d34495bb2d5
Smirrorgame <micha.halla@student.uni-luebeck.de>
2021-01-05 17:20:57 +0100

    added calibration algorithm which takes two ArrayLists of measurements and calculates the X and Y matrices based on the measurements. Added the use of the testDataGenerator, to calculate Matrices based on the generated data - but there's currently no useful output

--- src/robCalibration/QR24.java
+++ src/robCalibration/QR24.java
@@ -13,6 +13,7 @@ import org.apache.commons.math3.linear.QRDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealMatrixFormat;
 import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 
 import robprakt.Constants;
 import robprakt.graphics.Controller;
@@ -234,12 +235,30 @@ public class QR24 {
 		return true;
 	}
 	
-
 	public static void main(String[] args) {
 		double[][] doubleValues = {{1.89723489312,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 		RealMatrix matrix = new Array2DRowRealMatrix(doubleValues);
 		QR24 qr24 = new QR24(new Controller(null));
-		qr24.print(qr24.getTrans(matrix));
+		
+		ArrayList<ArrayList<RealMatrix>> measurements = new testDataGenerator().generateTestData(400);
+		ArrayList<RealMatrix> M = measurements.get(0);
+		ArrayList<RealMatrix> X = measurements.get(1);
+		ArrayList<RealMatrix> Y = measurements.get(2);
+		ArrayList<RealMatrix> N = measurements.get(3);
+		RealMatrix[] calc_XY = qr24.calibrate(M, N);
+		
+		RealMatrix genX = X.get(0);
+		RealMatrix genY = Y.get(0);
+
+		System.out.println("calcX1");
+		qr24.printTable(calc_XY[0]);
+//		System.out.println("calcX2");
+//		qr24.printTable(calc_XY[2]);
+		
+		System.out.println("calcY1");
+		qr24.printTable(calc_XY[1]);
+//		System.out.println("calcY2");
+//		qr24.printTable(calc_XY[3]);
 		
 	}
 	
@@ -298,6 +317,81 @@ public class QR24 {
 		printTable(Y);
 	}
 	
+	/**
+	 * This Method takes the measured matrices set M and N and creates a linear equation system to solve
+	 * for matrices X and Y
+	 * @param M consisting of Mi
+	 * @param N consisting if Ni
+	 * @return An array containing the matrix M and N, leading with M 
+	 */
+	public RealMatrix[] calibrate(ArrayList<RealMatrix> M, ArrayList<RealMatrix> N) {
+				
+		RealMatrix A = new Array2DRowRealMatrix(12*M.size(), 24);
+		RealVector B = new ArrayRealVector(12*M.size());
+
+		RealMatrix[] Rob_measurements = new RealMatrix[M.size()];
+		RealMatrix[] Track_measurements = new RealMatrix[M.size()];
+		
+		// Hier werden die Eintraege fuer die M und N Matrizen erstellt/kopiert
+		for(int i=0;i<M.size();i++) {
+			Track_measurements[i] = N.get(i);
+			Rob_measurements[i] = M.get(i);
+		}
+		
+		// Hier werden aus den Messungen Mi und Ni die Koeffizienten-Matrix A und der
+		// Loesungsvektor B generiert
+		for(int i=0;i<M.size();i++) {
+			A.setSubMatrix(createAEntry(Rob_measurements[i], Track_measurements[i]).getData(), i*12, 0);
+			B.setSubVector(i*12, new ArrayRealVector(createBEntry(Rob_measurements[i])));
+		}
+		
+		OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
+		regression.newSampleData(B.toArray(), A.getData());
+		regression.setNoIntercept(true);
+		regression.newSampleData(B.toArray(), A.getData());
+		RealVector w1 = new ArrayRealVector(regression.estimateRegressionParameters());
+		
+		DecompositionSolver solver = new QRDecomposition(A).getSolver();
+		RealVector w2 = solver.solve(B);
+		
+		
+		RealMatrix X1 = getFromW(w1.getSubVector(12, 12));
+		RealMatrix Y1 = getFromW(w1.getSubVector(0, 12));
+		
+		RealMatrix X2 = getFromW(w2.getSubVector(12, 12));
+		RealMatrix Y2 = getFromW(w2.getSubVector(0, 12));
+		
+//		printTable(X);
+//		printTable(Y);
+		
+		return new RealMatrix[] {X1,Y1, X2, Y2};
+	}
+	
+	/**
+	 * Berechnet eine Transformationsmatrix anhand eines gegebenen Vektors 
+	 * @param w Vektor mit Einträgen der Matrix
+	 * @return die berechnete Matrix
+	 */
+	public RealMatrix getFromW(RealVector w) {
+		RealMatrix M = new Array2DRowRealMatrix(4,4);
+		
+		M.setEntry(0, 0, w.getEntry(0));
+		M.setEntry(1, 0, w.getEntry(1));
+		M.setEntry(2, 0, w.getEntry(2));
+		M.setEntry(0, 1, w.getEntry(3));
+		M.setEntry(1, 1, w.getEntry(4));
+		M.setEntry(2, 1, w.getEntry(5));
+		M.setEntry(0, 2, w.getEntry(6));
+		M.setEntry(1, 2, w.getEntry(7));
+		M.setEntry(2, 2, w.getEntry(8));
+		M.setEntry(0, 3, w.getEntry(9));
+		M.setEntry(1, 3, w.getEntry(10));
+		M.setEntry(2, 3, w.getEntry(11));
+		M.setEntry(3, 3, 1);
+		
+		return M;
+	}
+		
 	/**
 	 * create A matrix for a single pair of measuring data M and N
 	 * @param m pose matrix of robot
@@ -390,8 +484,8 @@ public class QR24 {
 
 	//TODO: print matrix by Moritz
 	public void printTable(RealMatrix rm) {
-		int colDimNumber = rm.getRowDimension();
-		int rowDimNumber = rm.getColumnDimension();
+		int colDimNumber = rm.getColumnDimension();
+		int rowDimNumber = rm.getRowDimension();
 		ArrayList<String> headers = new ArrayList<String>();
 		String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 		for(int colDim = 0; colDim < colDimNumber; colDim++) {
@@ -410,13 +504,68 @@ public class QR24 {
 		ct.printTable();
 	}
 	
-	
-	
-	
-	
-	
-	
-	
+	/**
+	 * Berechnet die Transformation von Roboter zu Endeffektor (bzw. Marker) anhand 
+	 * vordefinierter D-H-Parametergegebener und gegebener Winkel der Gelenke
+	 * @param a1 winkel des Gelenks 1 in Grad
+	 * @param a2 winkel des Gelenks 2 in Grad
+	 * @param a3 winkel des Gelenks 3 in Grad
+	 * @return
+	 */
+	public RealMatrix rob_H_M(double a1,double a2,double a3) {
+
+		double theta1 = Math.toRadians(90);
+		double theta2 = Math.toRadians(a1);
+		double theta3 = Math.toRadians(90+a2);
+		double theta4 = Math.toRadians(a3);
+
+		double alpha1 = Math.toRadians(90);
+		double alpha2 = Math.toRadians(90);
+		double alpha3 = Math.toRadians(90);
+		double alpha4 = Math.toRadians(0);
+		
+		double d1 = 0;
+		double d2 = 100;
+		double d3 = 0;
+		double d4 = 150;
+		
+		double r1 = 0;
+		double r2 = 0;
+		double r3 = 0;
+		double r4 = 0;
+		
+		double[][] rob_H_one = {
+				{Math.cos(theta1), -Math.sin(theta1)*Math.cos(alpha1), Math.sin(theta1)*Math.sin(alpha1), r1*Math.cos(theta1)},
+				{Math.sin(theta1), Math.cos(theta1)*Math.cos(alpha1), -Math.cos(theta1)*Math.sin(alpha1), r1*Math.sin(theta1)},
+				{0d, Math.sin(alpha1), Math.cos(alpha1), d1},
+				{0d, 0d, 0d, 1d}};
+		
+		double[][] one_H_two = {
+				{Math.cos(theta2), -Math.sin(theta2)*Math.cos(alpha2), Math.sin(theta2)*Math.sin(alpha2), r2*Math.cos(theta2)},
+				{Math.sin(theta2), Math.cos(theta2)*Math.cos(alpha2), -Math.cos(theta2)*Math.sin(alpha2), r2*Math.sin(theta2)},
+				{0d, Math.sin(alpha2), Math.cos(alpha2), d2},
+				{0d, 0d, 0d, 1d}};
+		
+		double[][] two_H_three = {
+				{Math.cos(theta3), -Math.sin(theta3)*Math.cos(alpha3), Math.sin(theta3)*Math.sin(alpha3), r3*Math.cos(theta3)},
+				{Math.sin(theta3), Math.cos(theta3)*Math.cos(alpha3), -Math.cos(theta3)*Math.sin(alpha3), r3*Math.sin(theta3)},
+				{0d, Math.sin(alpha3), Math.cos(alpha3), d3},
+				{0d, 0d, 0d, 1d}};
+		
+		double[][] three_H_marker = {
+				{Math.cos(theta4), -Math.sin(theta4)*Math.cos(alpha4), Math.sin(theta4)*Math.sin(alpha4), r4*Math.cos(theta4)},
+				{Math.sin(theta4), Math.cos(theta4)*Math.cos(alpha4), -Math.cos(theta4)*Math.sin(alpha4), r4*Math.sin(theta4)},
+				{0d, Math.sin(alpha4), Math.cos(alpha4), d4},
+				{0d, 0d, 0d, 1d}};
+		
+		RealMatrix ROB_H_ONE = new Array2DRowRealMatrix(rob_H_one);
+		RealMatrix ONE_H_TWO = new Array2DRowRealMatrix(one_H_two);
+		RealMatrix TWO_H_THREE = new Array2DRowRealMatrix(two_H_three);
+		RealMatrix THREE_H_MARKER = new Array2DRowRealMatrix(three_H_marker);
+
+		RealMatrix ROB_H_MARKER = ONE_H_TWO.multiply(TWO_H_THREE).multiply(THREE_H_MARKER);
+		return ROB_H_MARKER;//ROB_H_ONE.multiply(ONE_H_TWO).multiply(TWO_H_THREE).multiply(THREE_H_MARKER);
+	}
 	
 	
 	
