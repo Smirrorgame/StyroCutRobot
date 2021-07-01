c85fada405e7d8cde8816522c6e1f33b39920ee5
Moritz Franz <Moritz.Franz@outlook.com>
2021-07-02 00:17:24 +0200

    Implementierung des grundlegenden Schnittalgorithmus abgeschlossen. Folgende Punkte sind implementiert: 1. Drehungswinkel um die z-Achse bestimmen, sodass die Normale in negative y Richtung zeigt. (relativ zum initial geladenen Objekt) 2. neue Stellungsmatrix für den Halteroboter mithilfe des Drehungswinkels ermitteln 3. Halteroboter drehen 4. Bahnplanung -> Notwendige Schnittpunkte ermitteln 5. Abfahren der Bahnen
    Die Schnittstückentfernung wird vorerst ausgelassen.
    
    Bei der Initialisierung muss die eingelesene und sortierte Triangles Liste noch and CuttingLogic übergeben werden.
    
    Erste Tests können folgen.

--- src/robprakt/cutting/CuttingLogic.java
+++ src/robprakt/cutting/CuttingLogic.java
@@ -39,6 +39,13 @@ public class CuttingLogic {
 	//TODO: muss beim Start auf true und beim Ende auf false gesetzt werden
 	
 	/**
+	 * neutralPositionCutterRobot used for trajectory calculations
+	 * contains position of neutral Position for cutting tool in homegeneous coordinates
+	 * relative to workspace (is being set in constructor)
+	 */
+	private RealVector neutralPositionCutterRobot;
+	
+	/**
 	 * The radius of the styrofoam cylinder in millimeters.
 	 * Currently assuming to have 200mm for the radius.
 	 */
@@ -103,13 +110,15 @@ public class CuttingLogic {
 	 * Is being called in constructor of CuttingLogic.
 	 */
 	private void setNeutralPosition() {
-		double offset = 100; // offset from the styrofoam cylinder (This is the gap between styrofoam and the cutting tool.)
+		double offset = 100d; // offset from the styrofoam cylinder (This is the gap between styrofoam and the cutting tool.)
 		//defining neutral position relative to workspace
 		double xValue = (-1d)*(this.radiusStyroCylinder + offset);
 		double yValue = (-1d)*(this.radiusStyroCylinder + offset);
 		double zValue = this.heightStyroHolder + this.heightStyroCylinder + offset;
 		//creating vector relative to workspace
 		RealVector neutralPositionVector = new ArrayRealVector(new double[] {xValue, yValue, zValue, 1});
+		//saving neutral position relative to workspace in CuttingLogic
+		this.neutralPositionCutterRobot = neutralPositionVector;
 		//saving neutral position in robotMovement
 		robotMovement.setNeutralPosition(neutralPositionVector);
 		}
@@ -133,6 +142,9 @@ public class CuttingLogic {
 	
 
 	//==========CUTTING==========
+	
+	//TODO: Derzeit ist die Entfernung von Schnittstücken nicht implementiert. Können wir vorerst weglassen,
+	//TODO: , damit man es später leichter hat die Fehler zu finden. Falls es denn welche gibt. ^^
 	
 	/**
 	 * Starts the cutting process.
@@ -164,6 +176,10 @@ public class CuttingLogic {
 			 *So the algorithm is not recursive. You always start with the reference pose.
 			 */
 			
+			//
+			//PREPARE FIRST CUT
+			//
+			
 			//calculate angle, so that the normal of the triangle points to negative y-axis (x-component of normal is zero)
 			double angle = rotationAngle(tr);
 			
@@ -176,8 +192,75 @@ public class CuttingLogic {
 			//move holder-robot into cutting position
 			robotMovement.moveMinChange(poseMatrixHolderRobot, this.transformCoords.getClientR2());
 			
+			//rotate Triangle so you can calculate with rotated vertices and rotated normal
+			Triangle rotTr = rotateTriangle(tr,rotationMatrix);
+			
 			//calculate vertices for trajectory for FIRST CUT
-			RealVector[] vertices = calculateVerticesFirstCut(rotationMatrix, tr);
+			RealVector[] vertices = calculateVerticesFirstCut(rotationMatrix, rotTr);
+			
+			//
+			//EXECUTE FIRST CUT
+			//
+			
+			//move from Neutral-Position to startPoint of the trajectory
+			robotMovement.moveCutterP2P(this.neutralPositionCutterRobot,vertices[0]);
+			
+			//cut through styro-foam
+			//start- to mid-point
+			robotMovement.moveCutterP2P(vertices[0], vertices[1]);
+			//mid- to end-point
+			robotMovement.moveCutterP2P(vertices[1], vertices[2]);
+			
+			//
+			//PREPARE FOR NEXT CUT
+			//
+			
+			//move to neutral position (potentially over auxiliary-position) 
+			this.moveToNeutralPosition(vertices[2]);
+			
+			//
+			//PREPARE FOR SECOND CUT
+			//
+			
+			//turn holder-robot by 180°
+			//calculate angle, so that the normal of the triangle points to positive y-axis (x-component of normal is zero)
+			//use angle that has been calculated before and add 180°
+			angle = angle + Math.PI;
+			
+			//calculate rotation matrix (used for end-effector pose calculation and transformation in of triangles in workspace)
+			rotationMatrix = this.calcRotationMatrix(angle);
+			
+			//calculate pose matrix for holder robot
+			poseMatrixHolderRobot = calcPoseHolderRobot(rotationMatrix);
+			
+			//move holder-robot into cutting position
+			robotMovement.moveMinChange(poseMatrixHolderRobot, this.transformCoords.getClientR2());
+			
+			//rotate Triangle so you can calculate with rotated vertices and rotated normal
+			rotTr = rotateTriangle(tr,rotationMatrix);
+			
+			//calculate vertices for trajectory for FIRST CUT
+			vertices = calculateVerticesFirstCut(rotationMatrix, rotTr);
+			
+			//
+			//EXECUTE SECOND CUT
+			//
+			
+			//move from Neutral-Position to startPoint of the trajectory
+			robotMovement.moveCutterP2P(this.neutralPositionCutterRobot,vertices[0]);
+			
+			//cut through styro-foam
+			//start- to mid-point
+			robotMovement.moveCutterP2P(vertices[0], vertices[1]);
+			//mid- to end-point
+			robotMovement.moveCutterP2P(vertices[1], vertices[2]);
+			
+			//
+			//PREPARE FOR NEXT CUT
+			//
+			
+			//move to neutral position (potentially over auxiliary-position) 
+			this.moveToNeutralPosition(vertices[2]);
 		}
 		
 		//set cutting status to not active
@@ -203,7 +286,11 @@ public class CuttingLogic {
 			throw new Exception("[CuttingLogic] Robot couldn't move into default pose.");
 	}
 	
-	
+	/**
+	 * Calculates the angle between negative y-axis and normal of the triangle. (normal is projected on x-y-plane)
+	 * @param tr
+	 * @return angle between negative y-axis and normal of the triangle (normal is projected on x-y-plane)
+	 */
 	private double rotationAngle(Triangle tr) {
 		
 		Vector3D normal = tr.getNormal();
@@ -217,6 +304,11 @@ public class CuttingLogic {
 		return 0;
 	}
 	
+	/**
+	 * Calculates homogeneous rotation matrix for rotation around z-axis.
+	 * @param angle
+	 * @return homogeneous rotation matrix
+	 */
 	private RealMatrix calcRotationMatrix(double angle) {
 		return new Array2DRowRealMatrix(new double[][] {
 			{1.0d,0.0d,0.0d,0.0d},
@@ -244,6 +336,24 @@ public class CuttingLogic {
 		return initialPoseHolderRobot.multiply(rotationMatrix);
 	}
 	
+	/**
+	 * Rotates the triangle around the z-axis.
+	 * @param tr is the input triangle
+	 * @param rotationMatrix is the rotation matrix that is used to rotate vertices and normal
+	 * @return rotated triangle
+	 */
+	private Triangle rotateTriangle(Triangle tr, RealMatrix rotationMatrix) {
+		return 	new Triangle(new Vector3D(rotationMatrix.operate(tr.getVertices()[0].toArray())),
+				new Vector3D(rotationMatrix.operate(tr.getVertices()[1].toArray())),
+				new Vector3D(rotationMatrix.operate(tr.getVertices()[2].toArray())));
+	}
+	
+	/**
+	 * Calculates the "striking" points of the trajectory relative to workspace.
+	 * @param rotationMatrix
+	 * @param tr
+	 * @return array of Vector3D of length 3 with start- mid- and end-point
+	 */
 	private RealVector[] calculateVerticesFirstCut(RealMatrix rotationMatrix, Triangle tr){
 		//defining vertices that surround the projection of the cylinder on the shifted yz-plane
 		/*
@@ -255,8 +365,9 @@ public class CuttingLogic {
 		Vector3D topRight= new Vector3D(-this.radiusStyroCylinder,this.radiusStyroCylinder,this.heightStyroCylinder+this.heightStyroHolder+this.assemblyZoneHeight);
 		
 		//defining triangle plane
-		Vector3D[] pointOfTriangle = tr.getVertices();
-		Plane plane = new Plane(pointOfTriangle[0], tr.getNormal(), Math.pow(10.0d, -10.0d));
+		Vector3D[] pointsOfTriangle = tr.getVertices();
+		
+		Plane plane = new Plane(pointsOfTriangle[0], tr.getNormal(), Math.pow(10.0d, -10.0d));
 		
 		//defining lines for start- and end-points of a trajectory for a cut
 		Line leftLine = new Line(bottomLeft,topLeft,Math.pow(10.0d, -10.0d));
@@ -270,34 +381,109 @@ public class CuttingLogic {
 		Vector3D intersectTopLine = plane.intersection(topLine);
 		//Vector3D intersectBottomLine = plane.intersection(bottomLine);
 		
+		//FILTER
 		//identifying intersections which can be used as a start point
-		Vector3D startPoint = intersectLeftLine;
-		
-		//MIT NULL ARBEITEN!!!!!!!!!!!!!!!!!!!!!!
-		
-		if((intersectLeftLine.getZ() >= bottomLeft.getZ() && intersectLeftLine.getZ() <= topLeft.getZ())) {
-			if((intersectRightLine.getZ() >= bottomRight.getZ() && intersectRightLine.getZ() <= topRight.getZ())) {
-				//intersections on the left and right side
-				//choose intersection with highest z-component as a starting point
-				startPoint = (intersectLeftLine.getZ() >= intersectRightLine.getZ()) ? intersectLeftLine : intersectRightLine;
-			}
+		//filter out only relevant intersections that are positioned directly near the cylinder
+		//if the intersection is not relevant, set it to null
+		if(!(intersectTopLine.getZ() >= topLeft.getY() && intersectTopLine.getZ() <= topRight.getY()) && intersectTopLine != null) {
+			intersectTopLine = null; //top-line
+		}
+		if(!(intersectLeftLine.getZ() >= bottomLeft.getZ() && intersectLeftLine.getZ() <= topLeft.getZ()) && intersectLeftLine != null) {
+			intersectLeftLine = null; //left-line
+		}
+		if(!(intersectRightLine.getZ() >= bottomRight.getZ() && intersectRightLine.getZ() <= topRight.getZ()) && intersectRightLine != null) {
+			intersectRightLine = null; //right-line
 		}
 		
-		if((intersectTopLine.getZ() >= topLeft.getY() && intersectLeftLine.getZ() <= t.getY())) {
-			if(!(intersectLeftLine.getZ() >= bottomLeft.getZ() && intersectLeftLine.getZ() <= topLeft.getZ())) {
-				if(!(intersectRightLine.getZ() >= bottomRight.getZ() && intersectRightLine.getZ() <= topRight.getZ())) {
-					//intersection only on top line
+		//start point of the trajectory
+		Vector3D startPoint = null;
+		
+		//choose start point
+		if(intersectTopLine != null && intersectLeftLine == null && intersectRightLine == null) {
+			//just top-line
+			startPoint = intersectTopLine;
+		} else {
+			if(intersectTopLine != null) {
+				//top and left or right
+				if(intersectLeftLine != null && intersectRightLine == null) {
+					//top and left
 					startPoint = intersectTopLine;
+				}
+				if(intersectLeftLine == null && intersectRightLine != null) {
+					//top and right
+					startPoint = intersectTopLine;
+				}
+			} else {
+				if(intersectLeftLine != null && intersectRightLine == null) {
+					//only left
+					startPoint = intersectLeftLine;
+				}
+				if(intersectLeftLine == null && intersectRightLine != null) {
+					//only right
+					startPoint = intersectLeftLine;
 				}
 			}
 		}
 		
-		
-		if(!(intersectTopLine.getZ() >= topLeft.getY() && intersectLeftLine.getZ() <= t.getY())) {
-			if(!(intersectLeftLine.getZ() >= bottomLeft.getZ() && intersectLeftLine.getZ() <= topLeft.getZ())) {
-				
+		//search for triangle vertex, that is the lowest
+		Vector3D lowestTriangleVertex = pointsOfTriangle[0];
+		if(pointsOfTriangle[1].getZ() <= lowestTriangleVertex.getZ()) {
+			lowestTriangleVertex = pointsOfTriangle[1];
+			if(pointsOfTriangle[2].getZ() <= lowestTriangleVertex.getZ()) {
+				lowestTriangleVertex = pointsOfTriangle[2];
 			}
 		}
-		return null;
+		
+		//calculate midpoint (tool stops at midpoint - it's the lowest point of the triangle)
+		//after midpoint the tool leaves in the positive or negative y-direction
+		
+		//project lowest vertex in shifted z-y-plane
+		Vector3D midPoint = new Vector3D(-this.radiusStyroCylinder,lowestTriangleVertex.getY(),lowestTriangleVertex.getZ());
+		
+		//calculate end-point of trajectory
+		//end-point is located on left or right line
+		//determining which on which end-point is located by normal of triangle
+		
+		//triangle surface points to the left
+		Vector3D endPoint = new Vector3D(midPoint.getX(),-this.radiusStyroCylinder,midPoint.getZ());
+		if (tr.getNormal().getY() > 0) {
+			//triangle surface points to the right
+			endPoint = new Vector3D(midPoint.getX(),this.radiusStyroCylinder,midPoint.getZ());
+		} else if(tr.getNormal().getY() == 0) {
+			//triangle is parallel to x-y-plane
+			//cuts horizontal from left to right, or from right to left
+			endPoint = startPoint.getY() > 0 ? 	new Vector3D(midPoint.getX(),-this.radiusStyroCylinder,midPoint.getZ()) :
+												new Vector3D(midPoint.getX(),this.radiusStyroCylinder,midPoint.getZ());
+		}
+		
+		//converting points to homogeneous RealVectors
+		RealVector v0 = new ArrayRealVector(new double[] {0.0d,0.0d,0.0d,1.0d,});
+		RealVector v1 = new ArrayRealVector(new double[] {0.0d,0.0d,0.0d,1.0d,});
+		RealVector v2 = new ArrayRealVector(new double[] {0.0d,0.0d,0.0d,1.0d,});
+		v0.setSubVector(0, new ArrayRealVector(startPoint.toArray()));
+		v1.setSubVector(0, new ArrayRealVector(midPoint.toArray()));
+		v2.setSubVector(0, new ArrayRealVector(endPoint.toArray()));
+		
+		
+		return new RealVector[] {v0,v1,v2};
+	}
+	
+	/**
+	 * Decides whether the cutter robot moves straight to neutral position,
+	 * or first to auxiliary and then neutral position.
+	 * Gets called after each cut through the styro-foam.
+	 * @param lastPosition
+	 */
+	private void moveToNeutralPosition(RealVector lastPosition) {
+		//move to neutral position
+		//check if last position of cutter robot was on left or right side
+		if(lastPosition.getEntry(1) < 0) {
+			//y-component is negative --> move straight to neutral
+			robotMovement.moveToNeutralPosition();
+		} else {
+			//y-component is positive --> move to auxiliary and then to neutral position
+			robotMovement.moveToAuxiliaryPosition();
+			robotMovement.moveToNeutralPosition();
+		}
 	}
 }
